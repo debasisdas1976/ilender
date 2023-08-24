775d7fe4c440adb7bb556bb68b4cd7817a1125e9
Debasis Das <debasis.das@gmail.com>
2023-08-24 11:00:11 +0530

    Add files via upload

---
+++ others/load-test-scripts/config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+##### ---------------------------------------------------------------
+###### KAFKA EVENTS
+##### ---------------------------------------------------------------
+
+NAMESPACE=cp4waiops
+KAFKA_TOPIC_FOR_EVENTS=cp4waiops-cartridge-alerts-noi-uwlw23hs
+
+SASL_USERNAME=cp4waiops-cartridge-kafka-auth-0
+
+SASL_PASSWORD=rdCkFQDhFbTM
+SEC="-X security.protocol=SASL_SSL -X ssl.ca.location=./data/ca.crt -X sasl.mechanisms=SCRAM-SHA-512 -X sasl.username=cp4waiops-cartridge-kafka-auth-0 -X sasl.password=rdCkFQDhFbTM"
+BROKER="iaf-system-kafka-bootstrap-cp4waiops.itzroks-5500028307-moy5le-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud:443"
+
+##### ---------------------------------------------------------------
+###### Application URL
+##### ---------------------------------------------------------------
+API_URL=http://ilender-frontweb-ilender-ns.turbonomic-demo-2bef1f4b4097001da9502000c44fc2b2-0000.us-east.containers.appdomain.cloud
+API_URL_CREDIT_SCORE=ilender-creditscore-ilender-ns.turbonomic-demo-2bef1f4b4097001da9502000c44fc2b2-0000.us-east.containers.appdomain.cloud/creditscore
+
+
+##### ---------------------------------------------------------------
+###### Memory Leak Param
+##### ---------------------------------------------------------------
+### memory consumption for each request
+export increaseSizeInKB=500
+export memoryMaxPercentage=99
+
+##### ---------------------------------------------------------------
+###### Load using locust
+##### ---------------------------------------------------------------
+export P_USERS=100
+export P_USER_SPAWN_RATE=5
+export P_TIME_DURATION=4m
+# export P_TIME_DURATION=2m
+
+export P_HOST=$API_URL
+export P_LOG=ERROR
+export P_RUN_MODE=inferencing
+
+
--- others/load-test-scripts/readme.md
+++ others/load-test-scripts/readme.md
@@ -1,1 +1,24 @@
-This folder contains the scripts to induce load to the creditscore service.
+# Demo Script
+
+Need to update few properties in the `config.sh`.
+
+#### 1. Update API_URL Property
+
+Update the API_URL property with application url.
+
+```
+API_URL=http://1.1.1.1:30600
+```
+
+Refer : [20-application-installation](../20-application-installation) to get application url
+
+#### 2. Update API_URL_CREDIT_SCORE Url
+
+Update the API_URL_CREDIT_SCORE property with creditscore service url.
+
+```
+API_URL_CREDIT_SCORE=http://1.1.1.1:30601
+```
+
+Refer : [20-application-installation](../20-application-installation) to get creditscore url
+
